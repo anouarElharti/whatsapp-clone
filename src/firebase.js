23139f23f8ea2ed5077610cbc06c9639f6f0fd5b
Anouar Elharti <ElhartAn@basfad.basf.net>
2022-06-10 18:07:07 +0100

    Initial commit local

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaAg3ldmesU01-Y4yCsn_I_TN5L794q1I",
+  authDomain: "whatsapp-clone-b41d6.firebaseapp.com",
+  projectId: "whatsapp-clone-b41d6",
+  storageBucket: "whatsapp-clone-b41d6.appspot.com",
+  messagingSenderId: "493626570168",
+  appId: "1:493626570168:web:a2c9397ca13c0ad2168b4d",
+};
+// Deploy
+//firebase deploy --only hosting:whatsapp-clone-anouar
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
