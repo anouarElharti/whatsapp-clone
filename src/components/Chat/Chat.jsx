635f2e5cde9d93091a3816768431fc7861ebbba0
Anouar Elharti <ElhartAn@basfad.basf.net>
2022-06-11 20:56:39 +0100

    Finishing up the WhatsApp clone UI

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -4,12 +4,21 @@ import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Avatar, IconButton } from "@mui/material";
 import "./Chat.css";
+import { InsertEmoticon, Mic } from "@material-ui/icons";
 
 const ChatRoom = () => {
   const [seed, setSeed] = useState("");
+  const [inputMessage, setMessage] = useState("");
+
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const handleMessage = (e) => {
+    e.preventDefault();
+    // FIREBASE SENDING MESSAGE
+  };
+
   return (
     <div className="chatRoom">
       <div className="chatHeader">
@@ -30,8 +39,31 @@ const ChatRoom = () => {
           </IconButton>
         </div>
       </div>
-      <div className="chatBody"></div>
-      <div className="chatFooter"></div>
+      <div className="chatBody">
+        <p className="chatMessage">
+          Hello from whatsapp
+          <span className="chatName">Anouar</span>
+          <span className="chatTimestamp">3:52pm</span>
+        </p>
+        <p className="chatMessage chatReciever">
+          Hello from whatsapp
+          <span className="chatName">Anouar</span>
+          <span className="chatTimestamp">3:52pm</span>
+        </p>
+      </div>
+      <div className="chatFooter">
+        <InsertEmoticon />
+        <form onSubmit={handleMessage}>
+          <input
+            value={inputMessage}
+            onChange={(e) => setMessage(e.target.value)}
+            type="text"
+            placeholder="Tape your message here"
+          />
+          <button type="submit">Send</button>
+        </form>
+        <Mic />
+      </div>
     </div>
   );
 };
